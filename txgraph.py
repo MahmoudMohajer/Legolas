260c7272e49d7560b93e0b3461cbc087b89c1234
MahmoudMohajer <mahmohajer3@gmail.com>
2023-02-01 13:54:17 +0430

    refactoring code

--- txgraph.py
+++ txgraph.py
@@ -1,28 +1,25 @@
-from neo4j import GraphDatabase
 from etherscan import Transactions
 
 class TxWriter:
     def __init__(self, session, api_key, address):
-        self.session = session  # neo4j driver session
+        self.session = session
         self.api_key = api_key
         self.address = address
-    def relation(self):
-        account = Transactions(self.api_key, self.address).normal_txns()
-        relations = []
-        for tx in account:
-            relations.append((tx['from'], tx['to']))
-        return relations
-    
-    def dbWriter(self, to_address, from_address):
+        self.relations = self.get_relations()
         
-        self.session.run(
-            """MERGE (a:Account {address: $fromAddress})
-            MERGE (b:Account {address: $toAddress})
-            MERGE (a)-[:SENT]->(b)""",
-            {"fromAddress": from_address, "toAddress": to_address}
-        )
+    def get_relations(self):
+        account = Transactions(self.api_key, self.address).normal_txns()
+        relations = [(tx['from'], tx['to']) for tx in account]
+        return relations
+        
+    def write_to_db(self, to_address, from_address):
+        query = """
+        MERGE (a:Account {address: $fromAddress})
+        MERGE (b:Account {address: $toAddress})
+        MERGE (a)-[:SENT]->(b)
+        """
+        self.session.run(query, {"fromAddress": from_address, "toAddress": to_address})
     
-    def writer(self):
-        for rel in self.relation():
-            self.dbWriter(rel[0], rel[1])
-
+    def write(self):
+        for from_address, to_address in self.relations:
+            self.write_to_db(to_address, from_address)
